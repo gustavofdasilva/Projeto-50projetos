a446265f732a2fb9e74a026bfc2e0b12fac9fb81
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-03-16 11:05:02 -0300

    Tentando continuar a rotação dos botões

--- rotating-navigation3/script.js
+++ rotating-navigation3/script.js
@@ -1,11 +1,23 @@
 let mainElement = document.getElementById("main")
+let buttonMenu = document.getElementById("menu")
+let closeMenu = document.getElementById("close")
+let deg = 0
+
+closeMenu.style.transform = `rotate${deg+25}deg`
 
 function rotate(dir) {
     console.log("clique")
     if(dir == "ccw") {
         console.log("para a direita")
-        mainElement.classList.add("ccw")
+        deg = -25
+        mainElement.style.transform = `rotate(${deg}deg)`
+        buttonMenu.style.transform = `rotate(${deg-25}deg)`
+        closeMenu.style.transform = `rotate(${25+deg}deg)`
     } else {
-        mainElement.classList.remove("ccw")
+        deg = 0
+        buttonMenu.style.transform = `rotate(${deg}deg)`
+        mainElement.style.transform = `rotate(${deg}deg)`
+        closeMenu.style.transform = `rotate(${25+deg}deg)`
     }
+    console.log(deg)
 }
