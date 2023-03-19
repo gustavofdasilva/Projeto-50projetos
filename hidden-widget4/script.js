8883a6507b95f76a7aea280d636f4b922084fb3b
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-03-19 14:00:15 -0300

    Finalizando projeto 4!

---
+++ hidden-widget4/script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button")
+let input = document.getElementById("busca")
+let div = document.getElementById("search")
+let closed = true
+
+function hiddenWidget() {
+    div.classList.toggle("active")
+}
