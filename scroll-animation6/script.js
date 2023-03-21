2d57a7a52030d5233ebf8ec9c31c0ef4b8cd9758
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-03-20 21:29:26 -0300

    Finalizando projeto 6!

--- scroll-animation6/script.js
+++ scroll-animation6/script.js
@@ -1,5 +1,13 @@
 const divs = document.getElementsByClassName("div")
+let elementHeight = 260
 
 window.addEventListener('scroll',(e) => {
-    console.log(window.scrollY) 
+    for (let i = 0; i < divs.length ; i++) {
+        let elementTop = divs[i].getBoundingClientRect().top
+        let windowTop = window.innerHeight
+        if(elementTop < windowTop - elementHeight) {
+            divs[i].classList.add("visible")
+        } else {divs[i].classList.remove("visible")}
+    }
+
 })
