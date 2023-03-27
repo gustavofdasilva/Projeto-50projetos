c51324955f1803f45195096c298293062ddcbf39
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-03-26 21:05:49 -0300

    Finalizando projeto!

--- form-wave8/script.js
+++ form-wave8/script.js
@@ -2,13 +2,31 @@ let emailWord = document.getElementsByClassName("email")
 let passWord = document.getElementsByClassName("pass")
 
 let inputEmail = document.getElementById("email")
+    let isEmailFocus = document.activeElement === inputEmail
 let inputPass = document.getElementById("password")
 
 
 
-inputEmail.addEventListener('focus',(e) => {
+inputEmail.addEventListener('focusin',(e) => {
     for(let i = 0 ; i < emailWord.length ; i++) {
-            console.log(emailWord[i])
-            emailWord[i].classList.add("active")
+        emailWord[i].classList.add("active")
+    }
+})
+
+inputEmail.addEventListener('focusout',(e) => {
+    for(let i = 0 ; i < emailWord.length ; i++) {
+        emailWord[i].classList.remove("active")
+    }
+})
+
+inputPass.addEventListener('focusin',(e) => {
+    for(let i = 0 ; i < passWord.length ; i++) {
+        passWord[i].classList.add("active")
+    }
+})
+
+inputPass.addEventListener('focusout',(e) => {
+    for(let i = 0 ; i < passWord.length ; i++) {
+        passWord[i].classList.remove("active")
     }
 })
