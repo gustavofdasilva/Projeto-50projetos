38522a31a6db6db75a52ad5a38eab3cdbdce7a45
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-04-12 21:28:35 -0300

    Finalizando projeto 12!

--- faq-collapse12/script.js
+++ faq-collapse12/script.js
@@ -5,9 +5,14 @@ function toggleInfo(id) {
     for(let i = 0; i < sections.length ; i++) {
         let section = sections[i]
         if(section.classList.contains(`s${id.toString()}`)) {
-            section.children[1].classList.toggle("nonvisible")
+            let button = section.children[0].children[0].children[1]
+            let span = section.children[0]
+
+            span.children[1].classList.toggle("nonvisible")
             section.classList.toggle("sec-visible")
-        }
+            section.children[0].classList.toggle("sec-visible")
+            button.classList.toggle("sec-visible")
+        } 
     }
 }
 
